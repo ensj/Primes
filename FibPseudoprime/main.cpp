05ff8f61a2573fd3d463771f6c143701b3626420
ensj <ensjtrans@gmail.com>
2021-01-26 23:11:46 -0500

    test 1

--- FibPseudoprime/main.cpp
+++ FibPseudoprime/main.cpp
@@ -1,18 +1,39 @@
 #include "nfunctions.hpp"
 #include <iostream>
 
+std::string vectToString(std::vector<Mod> v) {
+  std::string res = "{ ";
+
+  for (size_t i = 0; i < v.size(); i++) {
+    res += v[i].getString() + ", ";
+  }
+  res += "}";
+
+  return res;
+}
+
 int main() {
-  long n = 5563 * 2 * 3 * 5 * 7;
+  // long n = 2 * 7 * 7; // 5563 * 2 * 3 * 5 * 7;
 
-  while (true) {
-    long factor = ECFactor(n, 3000, 200);
+  // std::cout << vectToString(chooseEAndP(n)) << std::endl;
+  // std::cout << vectToString(chooseEAndP(n)) << std::endl;
+  // std::cout << vectToString(chooseEAndP(n)) << std::endl;
 
-    std::cout << "n: " << n << ", factor: " << factor << std::endl;
+  for (long n = 1; n < 100000; n++) {
+    std::cout << "factoring: " << n << std::endl;
 
-    if (factor == 1)
-      break;
+    long factorNum = n;
 
-    n = n / factor;
+    while (true) {
+      long factor = ECFactor(factorNum, 3000, 500);
+
+      std::cout << "  n: " << factorNum << ", factor: " << factor << std::endl;
+
+      if (factor == 1)
+        break;
+
+      factorNum = factorNum / factor;
+    }
   }
   return 0;
 }
